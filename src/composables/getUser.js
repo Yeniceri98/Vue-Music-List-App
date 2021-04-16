d516ffcffd3526c6275f354f19ba39310e5496f2
Ahmet Sami Yeniçeri <ahmetsamiyeniceri@gmail.com>
2021-04-16 15:45:35 +0300

    commit 4

---
+++ src/composables/getUser.js
@@ -0,0 +1,16 @@
+import { ref } from 'vue'
+import { projectAuth } from '../firebase/config'
+
+const user = ref(projectAuth.currentUser)
+
+// Auth changes
+projectAuth.onAuthStateChanged(_user => {
+    console.log('User state change. Current user is:', _user)
+    user.value = _user
+});
+
+const getUser = () => {
+    return { user } 
+}
+
+export default getUser
---
+++ src/composables/useCollection.js
@@ -0,0 +1,23 @@
+import { ref } from 'vue'
+import { projectFirestore } from '../firebase/config'
+
+const useCollection = (collection) => {
+    const error = ref(null)
+
+    // Add a new document
+    const addDoc = async (doc) => {
+        error.value = null
+
+        try {
+            await projectFirestore.collection(collection).add(doc)
+        }
+        catch(err) {
+            console.log(err.message)
+            error.value = 'could not send the message'
+        }
+    }
+
+    return { error, addDoc }
+}
+
+export default useCollection
