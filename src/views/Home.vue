81bff7e3070a000bfab2ed1b4b0dfbbbf873c53e
Ahmet Sami Yeniçeri <ahmetsamiyeniceri@gmail.com>
2021-04-18 11:52:41 +0300

    commit 17

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,21 @@
 <template>
     <div class="home">
-        
+        <div v-if="error" class="error">{{ error }}</div>
+        <div v-if="documents">
+            <div v-for="doc in documents" :key="doc.id">{{ doc.title }}</div>
+        </div>
     </div>
 </template>
 
 <script>
+import getCollection from '@/composables/getCollection'
 
 export default {
     name: 'Home',
-    components: {}
+    setup() {
+        const { error, documents } = getCollection('playlists')
+
+        return { error, documents }
+    }
 }
 </script>
